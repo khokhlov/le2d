017b718b2f43e8495aadead6d271dd95a8e480a8
Nikolay Khokhlov <kolya.khokhlov@gmail.com>
2012-10-15 13:10:00 +0400

    Test on Intel E5-2690

---
+++ tests/sse2/E5-2690/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+n=7000
+s0=16
+s=64
+
+bins=( le2d_base_f le2d_base_d le2d_f le2d_d le2d_f_sse le2d_d_sse le2d_f_avx le2d_d_avx )
+
+for i in ${bins[@]}
+do
+	echo "" > $i.log
+	cp ../../src/$i ./
+done
+
+for ((c=$s0;c<$n;c+=$s))
+do
+	for i in ${bins[@]}
+	do
+		echo $c $i
+		./$i $c $c $((2*$n/$c)) >> $i.log
+	done
+done
